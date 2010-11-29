716142feb575484669199aea3ec63d3833a4ac32
jsapardeveloper <jsapar.developer@gmail.com>
2010-11-29 15:53:49 +0000

    Added a miscellaneous Cell test class for tests that have no good classification, like: java cloning tests

---
+++ jsapar-exp-internal/src/test/java/exp/jsapar/types2/CellTestOnMiscellaneous.java
@@ -0,0 +1,14 @@
+package exp.jsapar.types2;
+
+/**
+ * Miscellaneous unit tests for testing Cell objects. A mixture of unit tests
+ * that have no particular classification and which can not be accommodated in
+ * other cell testing classes.
+ * 
+ * @author JsaPar Developer
+ */
+public class CellTestOnMiscellaneous {
+
+	// TODO add the tests
+	
+}
